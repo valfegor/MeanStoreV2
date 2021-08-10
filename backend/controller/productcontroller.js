83d654288eeb1c0ff8570d78bae3922468e8f510
Anthony Ahumada <locopala99@gmail.com>
2021-08-10 09:17:22 +0200

    more js

--- backend/controller/productcontroller.js
+++ backend/controller/productcontroller.js
@@ -2,5 +2,11 @@ const Product = require("../models/product");
 
 
 const registerProduct = async (req,res)=>{
+    if(!req.body.name || !req.body.code || !req.body.price) return res.status(401).send("You have to fill all the camps so please");
 
+    const existinProduct = await Product.findone({name:req.body.name , code:req.body.code});
+
+    if(existinProduct) return res.status(401).send("Sorry you have the same code and name");
+
+    
 }
